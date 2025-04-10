9af32573f864b3d1b7056ecbf4700e04386e3ead
MonicaDelCap <monicadelgado437@gmail.com>
2025-04-10 13:27:06 +0200

    scalar

--- ApiExamenMonicaDelgado/Program.cs
+++ ApiExamenMonicaDelgado/Program.cs
@@ -1,17 +1,21 @@
 using ApiExamenMonicaDelgado.Data;
 using ApiExamenMonicaDelgado.Repositories;
+
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<RepositorySeries>();
+builder.Services.AddDbContext<SeriesContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddTransient<RepositorySeries>();
-builder.Services.AddDbContext<SeriesContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlAzure")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,20 +23,16 @@ if (app.Environment.IsDevelopment())
 {
 }
 app.MapOpenApi();
-
-app.UseHttpsRedirection();
 app.MapScalarApiReference();
-//app.UseSwaggerUI(options =>
-//{
-//    options.RoutePrefix = "";
-//    options.SwaggerEndpoint("/openapi/v1.json", "Api Monica");
-//});
+app.UseHttpsRedirection();
 
 app.MapGet("/", context =>
 {
     context.Response.Redirect("/scalar");
     return Task.CompletedTask;
 });
+
+
 app.UseAuthorization();
 
 app.MapControllers();
